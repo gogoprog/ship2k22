be0654b24455f45f03b053ac1f215566f9062b34
Gauthier Billot <gogoprog@gmail.com>
2022-01-06 21:31:43 +0100

    Playable

--- src/Main.hx
+++ src/Main.hx
@@ -54,79 +54,6 @@ class Main {
         var state = 0;
         var score;
         var bestScore = 0;
-        ctx.font = "20px monospace";
-        function col(n) {
-            ctx.fillStyle = n;
-        }
-        function alpha(n) {
-            ctx.globalAlpha = n;
-        }
-        function scale(s) {
-            ctx.scale(s, s);
-        }
-        function drawRect(x:Float, y:Float, w, h) {
-            ctx.fillRect(x-w/2, y-h/2, w, h);
-        }
-        function mto(x, y) {
-            ctx.moveTo(x, y);
-        }
-        function lto(x, y) {
-            ctx.lineTo(x, y);
-        }
-        function beginPath() {
-            ctx.beginPath();
-        }
-        function fill() {
-            ctx.fill();
-        }
-        function circle(x, y, r) {
-            beginPath();
-            ctx.arc(x, y, r, 0, 6.28);
-            fill();
-        }
-        function drawShip(x:Float, y:Float) {
-            col("#ccd");
-            drawRect(x, y, 20, 40);
-            col("#669");
-            drawRect(x, y, 4, 8);
-            circle(x, y-20, 10);
-            col("gold");
-            drawRect(x, y + 20, 20, 4);
-
-            for(i in extremes) {
-                ctx.save();
-                ctx.transform(i, 0, 0, 1, x, y - 16);
-                col("#88d");
-                beginPath();
-                mto(10, 10);
-                lto(10, 30);
-                lto(20, 30);
-                fill();
-                ctx.restore();
-            }
-        }
-        function drawEnemy(x:Float, y:Float) {
-            col("red");
-            drawRect(x, y, 20, 40);
-            circle(x, y+20, 10);
-            col("#669");
-            drawRect(x, y, 4, 8);
-            col("gold");
-            drawRect(x, y - 20, 20, 4);
-
-            for(i in extremes) {
-                ctx.save();
-                ctx.transform(i, 0, 0, 1, x, y - 36);
-                col("#666");
-                beginPath();
-                mto(10, 30);
-                lto(10, 10);
-                lto(20, 10);
-                fill();
-                col("#a11");
-                ctx.restore();
-            }
-        }
         function random():Float {
             var x = (sin(rseed++) + 1) * 9999;
             return x - Std.int(x);
@@ -135,7 +62,7 @@ class Main {
             mustFire = untyped e.buttons;
         }
         w.onmousemove = function(e) {
-            mx = e.clientX;
+            mx = 512 - e.clientX;
         }
         function getn(arr:Dynamic) {
             var n = arr.length;
@@ -155,46 +82,63 @@ class Main {
             ctx.fillText(a, b, c);
         }
         function explode(x, y) {
-            for(j in 0...36) {
+            for(j in 0...16) {
                 particles[getn(particles)] = {x:x, y:y, t:0};
             }
 
             untyped z(1, .05, 652, 1, .01, .6, 4, 71, .9);
         }
         W.reset(c);
-        W.clearColor("#fff");
+        W.clearColor("#222");
         W.camera({x:256, y:256, z:496, rz:180, fov:27});
-        W.light({x:0.5, y:-1, z:-0.5});
-
+        W.light({x:0.5, y:1, z:-0.5});
         W.cube({n:"ship", x:0, y:460, z:0, size:32});
-
         W.cube({x:0, y:0, z:0, size:10});
         W.cube({x:512, y:0, z:0, size:10});
         W.cube({x:512, y:512, z:0, size:10});
         W.cube({x:0, y:512, z:0, size:10});
 
+        var max_enemies = 16;
+        var max_bullets = 48;
+        var max_bonuses = 16;
+        var max_particles = 64;
+        var max_stars = 32;
+
+        for(e in 0...max_enemies) {
+            W.cube({n:"e" + e, x:-10000, y:0, z:0, size:32, b:"#922"});
+        }
+
+        for(i in 0...max_bullets) {
+            W.cube({n:"b" + i, x:-10000, y:0, z:0, h:12, w:4, b:"#aaa"});
+        }
+
+        for(i in 0...max_stars) {
+            W.sphere({n:"s" + i, size:2});
+        }
+
+        for(i in 0...max_particles) {
+            W.sphere({n:"p" + i, size:2});
+        }
+
+        for(i in 0...max_bonuses) {
+            W.sphere({n:"bo" + i, size:6, b:"#2a2"});
+        }
+
         function loop(t:Float) {
-
-            W.move({n:"ship", x:512 - mx});
-            /*
-            col("#000");
-            drawRect(256, 256, screenSize, screenSize);
             rseed = 1;
-            col("#fff");
 
-            for(i in 0...99) {
-                drawRect(random() * screenSize, (random() * screenSize + t * (random() * 0.2)) % screenSize, 2, 2);
+            for(i in 0...max_stars) {
+                W.move({n:"s"+i, x:(random() * screenSize), y:((random() * screenSize + t * (random() * 0.2)) % screenSize)});
             }
 
             if(state == 0) {
-                scale(4);
-                ftext("SHIP2k", 24, 32);
-                scale(1/2);
-                drawShip(200, 160);
-                ftext("Click to play!", 42, 232);
-                scale(1/2);
-                ftext("Best score: " + bestScore, 32, 232);
-
+                /* scale(4); */
+                /* ftext("SHIP2k", 24, 32); */
+                /* scale(1/2); */
+                /* drawShip(200, 160); */
+                /* ftext("Click to play!", 42, 232); */
+                /* scale(1/2); */
+                /* ftext("Best score: " + bestScore, 32, 232); */
                 if(mustFire && time > 60) {
                     state++;
                     lastFireTime = time = score = 0;
@@ -206,63 +150,69 @@ class Main {
                     bonuses = [];
                 }
             } else if(state == 1) {
-                alpha(1);
+                for(i in 0 ... max_bullets) {
+                    var b = bullets[i];
 
-                for(b in bullets) {
-                    b.y += 10 * b.d;
-                    col(b.d == -1 ? "#0f0" : "red");
-                    drawRect(b.x, b.y, 4, 12);
+                    if(b!=null) {
+                        b.y += 10 * b.d;
+                        /* col(b.d == -1 ? "#0f0" : "red"); */
+                        W.move({n:"b"+i, x:b.x, y:b.y});
 
-                    if(b.d > 0) {
-                        if(abs(b.y - 420) + abs(b.x-mx) < 32) {
-                            life--;
-                            b.y = 999;
-                            alpha(0.5);
-                            untyped z(1, .05, 918, .8, .04, 0, .2, 24, .6);
+                        if(b.d > 0) {
+                            if(abs(b.y - 420) + abs(b.x-mx) < 32) {
+                                life--;
+                                b.y = 999;
+                                /* alpha(0.5); */
+                                untyped z(1, .05, 918, .8, .04, 0, .2, 24, .6);
 
-                            if(life < 1) {
-                                bestScore = cast m.max(score, bestScore);
-                                state = 0;
-                                time = 0;
+                                if(life < 1) {
+                                    bestScore = cast m.max(score, bestScore);
+                                    state = 0;
+                                    time = 0;
+                                }
                             }
                         }
-                    }
-                }
-
-                for(b in bonuses) {
-                    b.y += 3;
-                    var hit:Bool = false;
-
-                    if(abs(b.y - 450) + abs(b.x-mx) < 32) {
-                        b.y = 999;
-                        hit = true;
-                    }
-
-                    if(b.b==0) {
-                        col("#0f0");
-
-                        if(hit) {
-                            power++;
-                            untyped z(1, .05, 115, .5, .9, .7, .9, 6, .3);
-                        }
                     } else {
-                        col("#fff");
-
-                        if(hit) {
-                            life = cast m.min(life+1, 10);
-                            untyped z(1, .05, 1704, .2, .2, .2, .1, 0, .6);
-                        }
+                        W.move({n:"b"+i, x:-10000});
                     }
-
-                    circle(b.x, b.y, 8);
                 }
 
-                drawShip(mx, 460);
+                for(i in 0...max_bonuses) {
+                    var b = bonuses[i];
+
+                    if(b != null) {
+                        b.y += 3;
+                        var hit:Bool = false;
+
+                        if(abs(b.y - 450) + abs(b.x-mx) < 32) {
+                            b.y = 999;
+                            hit = true;
+                        }
+
+                        if(b.b==0) {
+                            if(hit) {
+                                power++;
+                                untyped z(1, .05, 115, .5, .9, .7, .9, 6, .3);
+                            }
+                        } else {
+                            if(hit) {
+                                life = cast m.min(life+1, 10);
+                                untyped z(1, .05, 1704, .2, .2, .2, .1, 0, .6);
+                            }
+                        }
+
+                        W.move({n:"bo"+i, x:b.x, y:b.y});
+                    } else {
+                        W.move({n:"bo"+i, x:-10000 });
+                    }
+                }
+
+                W.move({n:"ship", x:mx});
 
                 if(mustFire) {
                     if(time - lastFireTime > 15 - power/2) {
                         for(i in 0...cast power/2) {
-                            fire(mx - power*2 + i * 8, 435, -1);
+                            fire((mx) - power*2 + i * 8, 435, -1);
                         }
 
                         lastFireTime = time;
@@ -270,77 +220,80 @@ class Main {
                     }
                 }
 
-                col("#d33");
-
-                for(i in 0...particles.length) {
+                for(i in 0...max_particles) {
                     var p = particles[i];
-                    p.t++;
-                    var angle = i * 0.174;
-                    var v = random() * 3;
-                    p.x += cos(angle) * v;
-                    p.y += sin(angle) * v;
-                    drawRect(p.x, p.y, cast v, cast v);
+
+                    if(p != null) {
+                        p.t++;
+                        var angle = i * 0.174;
+                        var v = random() * 3;
+                        p.x += cos(angle) * v;
+                        p.y += sin(angle) * v;
+                        W.move({n:"p"+i, x:p.x, y:p.y});
+                    } else {
+                        W.move({n:"p"+i, x:-10000 });
+                    }
                 }
 
                 rseed = time;
 
-                for(e in enemies) {
-                    var x = e.x + sin(++e.t / 99) * 99;
-                    var y = -64 + e.t;
+                for(i in 0...max_enemies) {
+                    var e = enemies[i];
 
-                    if(e.t % 60 < time/2000) {
-                        untyped z(1, .05, 48, .1, .4, 5, 0, 84, .5);
-                        fire(x, y, 1);
-                    }
+                    if(e != null) {
+                        var x = e.x + sin(++e.t / 99) * 99;
+                        var y = -64 + e.t;
 
-                    alpha(1);
+                        if(e.t % 60 < time/2000) {
+                            untyped z(1, .05, 48, .1, .4, 5, 0, 84, .5);
+                            fire(x, y, 1);
+                        }
 
-                    for(b in bullets) {
-                        if(b.d < 0) {
-                            if(abs(b.y - y) + abs(b.x-x) < 32) {
-                                b.y = -999;
-                                e.life -= 1;
-                                alpha(0.5);
-                                untyped z(1, .05, 179, .1, .5, 3.5, 1.7, 80, .6);
+                        for(b in bullets) {
+                            if(b.d < 0) {
+                                if(abs(b.y - y) + abs(b.x-x) < 32) {
+                                    b.y = -999;
+                                    e.life -= 1;
+                                    untyped z(1, .05, 179, .1, .5, 3.5, 1.7, 80, .6);
 
-                                if(e.life < 1) {
-                                    e.t = 666;
-                                    score += 100;
-                                    explode(x, y);
+                                    if(e.life < 1) {
+                                        e.t = 666;
+                                        score += 100;
+                                        explode(x, y);
 
-                                    if(random() < 0.6) {
-                                        bonuses[getn(bonuses)] = {x:x, y:y, b:time%2};
+                                        if(random() < 0.6) {
+                                            bonuses[getn(bonuses)] = {x:x, y:y, b:time%2};
+                                        }
+
+                                        break;
                                     }
-
-                                    break;
                                 }
                             }
                         }
-                    }
 
-                    drawEnemy(x, y);
+                        W.move({n:"e"+i, x:x, y:y});
+                    } else {
+                        W.move({n:"e"+i, x:-10000});
+                    }
                 }
 
                 if((time % 150) < time/2000) {
                     enemies[getn(enemies)] = {x: screenSize * random(), t:0, life:5};
                 }
 
-                alpha(1);
-                col("#222");
-                drawRect(256, 500, screenSize, 24);
-                col("#aaf");
-                var str = "";
-
-                for(i in 0...10) {
-                    str += i < life ? untyped ws : untyped bs;
-                }
-
-                ftext(str, 12, 506);
-                ftext(cast score, 400, 506);
-                ftext(cast power, 166, 506);
+                /* alpha(1); */
+                /* col("#222"); */
+                /* drawRect(256, 500, screenSize, 24); */
+                /* col("#aaf"); */
+                /* var str = ""; */
+                /* for(i in 0...10) { */
+                /*     str += i < life ? untyped ws : untyped bs; */
+                /* } */
+                /* ftext(str, 12, 506); */
+                /* ftext(cast score, 400, 506); */
+                /* ftext(cast power, 166, 506); */
             }
 
-            */
             time++;
             w.requestAnimationFrame(loop);
         }
